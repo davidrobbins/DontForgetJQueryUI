67254c42648551a25f661812ad42f29ddc3f619a
David Robbins <dave@wakanda.org>
2013-05-01 16:42:37 -0700

    added request handler for generic jQUery UI autocomplete support.

---
+++ DontForgetJQueryUI/scripts/bootstrap.js
@@ -0,0 +1,1 @@
+﻿//Start our jQuery UI autocomplete request handler.addHttpRequestHandler('/jqueryAutoComplete', 'scripts/jqueryAutoComplete.js', 'jqueryAutoComplete' );
---
+++ DontForgetJQueryUI/scripts/jqueryAutoComplete.js
@@ -0,0 +1,23 @@
+﻿function jqueryAutoComplete(request, response)
+{
+	var result = [];
+	var url = request.url;
+	var path = getURLPath(url);
+	var params = getURLQuery(url);
+	var classname = path[1];
+	var attname = path[2];
+	var value = params.term;
+	
+	if (value != null && attname != null && classname != null)
+	{
+		var arr = ds[classname].query(attname+" == :1", value+"*").toArray(attname, "withKey", 0,20);
+		arr.forEach(function(elem) {
+			var v = elem[attname];
+			result.push({id: elem.__KEY, label:v, value:v});
+		});
+	}
+	
+	response.contentType = 'application/json';
+    response.body = JSON.stringify(result);
+}
+
